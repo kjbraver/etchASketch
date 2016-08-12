2a595575066ab081ea2022f928b176b707db33a9
Kyle Braver <kyle.braver@gmail.com>
2016-08-11 23:54:58 -0700

    Added game menu section to webpage. Menu now includes a button which allows user to dynamically change the size of the game board. leaveColorTrail method currently broken after resizing game board.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
-	//generate 16 row divs for gameBoard
-	for(var i = 0; i < 16; i++) {
-		$('#gameBoard').append($('<div class="row"></div>'));
-	}
 
-	//generate 16 column divs in each row to fill out gameBoard
-	for(var j = 0; j < 16; j++) {
-		$('.row').append($('<div class="cell"></div>'));
-	}
+	generateBoard(16);
+
+	//resize game board when button is clicked
+	$('#resize').on('click', function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		var boardSize;
+
+		do {
+		var userInput = prompt("How big would you like the new board to be?", "Ex: Enter 2 for a 2x2 board.")
+		boardSize = parseInt(userInput);
+		} while(isNaN(boardSize));
+
+		//calculate new length and width of cells
+		var cellBorder = 2.0 * parseFloat($('.cell').css('border-width'));
+		var gameBoardLength = 600.0;
+		var cellLength = gameBoardLength/boardSize;
+
+		generateBoard(boardSize);
+
+		//update cell class css with new length and width
+		$('.cell').width(cellLength - cellBorder);
+		$('.cell').height(cellLength - cellBorder);
+		$('.row').height(cellLength);
+
+	}); 
 
 	leaveColorTrail('red');
+
 });
 
 //leaves color trail of color "newColor"
@@ -21,4 +40,25 @@ function leaveColorTrail(newColor) {
 		event.preventDefault();
 		$(this).addClass(newColor);	
 	});
+}
+
+//generates game board with boardSize x boardSize dimensions
+//boardSize: int
+function generateBoard(boardSize) {
+	if(boardSize >= 1) {
+		//empty current gameBoard
+		$('#gameBoard').empty();
+
+		//generate 'boardSize' row divs for gameBoard
+		var $gameBoard = $('#gameBoard');
+		for(var i = 0; i < boardSize; i++) {
+			$gameBoard.append($('<div class="row"></div>'));
+		}
+
+		//generate 'boardSize' column divs in each row to fill out gameBoard
+		var $row = $('.row');
+		for(var j = 0; j < boardSize; j++) {
+			$row.append($('<div class="cell"></div>'));
+		}
+	}
 }
